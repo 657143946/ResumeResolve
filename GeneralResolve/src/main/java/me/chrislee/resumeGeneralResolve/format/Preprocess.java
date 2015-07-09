7774f61e23385bf42c52acada685a6599b101745
chrislee <657143946@qq.com>
2015-07-09 13:44:23 +0800

    Formatter -> Preprocess

--- GeneralResolve/src/main/java/me/chrislee/resumeGeneralResolve/format/Preprocess.java
+++ GeneralResolve/src/main/java/me/chrislee/resumeGeneralResolve/format/Preprocess.java
@@ -4,9 +4,9 @@ package me.chrislee.resumeGeneralResolve.format;
  * Created by ChrisLee.
  * 文本格式化 接口
  */
-public interface Format {
+public interface Preprocess {
     /**
      * 文本预处理格式化
      */
-    public String format(String content);
+    public String preprocess(String content);
 }
