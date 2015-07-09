392d0f7e41212b1663b5e9afe7246dd0a0eafd87
chrislee <657143946@qq.com>
2015-07-09 17:15:23 +0800

    Tika read实现

--- GeneralResolve/src/main/java/me/chrislee/resumeGeneralResolve/read/TikaReader.java
+++ GeneralResolve/src/main/java/me/chrislee/resumeGeneralResolve/read/TikaReader.java
@@ -1,5 +1,7 @@
 package me.chrislee.resumeGeneralResolve.read;
 
+import org.apache.log4j.Logger;
+import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
@@ -9,101 +11,67 @@ import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
+import java.io.*;
 
 /**
  * Created by ChrisLee.
  * Tika读取文档简历
  */
 public class TikaReader implements Read {
-
-    public String read(String fileAbsolutePath) {
-        String content = "";
-        try {
-            Parser parser = new AutoDetectParser();
-            InputStream input = null;
-            File file = new File(fileAbsolutePath);
-            Metadata metadata = new Metadata();
-            metadata.set(Metadata.CONTENT_ENCODING, "utf-8");
-            metadata.set(Metadata.RESOURCE_NAME_KEY, file.getName());
-            input = new FileInputStream(file);
-            ContentHandler handler = new BodyContentHandler();//当文件大于100000时，new BodyContentHandler(1024*1024*10);
-            ParseContext context = new ParseContext();
-            context.set(Parser.class,parser);
-            parser.parse(input,handler, metadata,context);
-
-
-            try {
-                metadataToString(metadata);
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            content = handler.toString();
-        } catch (IOException e) {
-            throw new IllegalArgumentException("file not found："+fileAbsolutePath);
-        } catch (TikaException e) {
-            System.out.println("不能解析");
-//			throw new IllegalArgumentException("can't read file through Tika："+fileAbsolutePath);
-        } catch (SAXException e) {
-            throw new IllegalArgumentException("can't parse file through Tika："+fileAbsolutePath);
-        }
-        return content;
-
-    }
-    public String read(File file) {
-        String content = "";
-        try {
-            Parser parser = new AutoDetectParser();
-            InputStream input = null;
-            Metadata metadata = new Metadata();
-            metadata.set(Metadata.CONTENT_ENCODING, "utf-8");
-            metadata.set(Metadata.RESOURCE_NAME_KEY, file.getName());
-            input = new FileInputStream(file);
-            ContentHandler handler = new BodyContentHandler();//当文件大于100000时，new BodyContentHandler(1024*1024*10);
-            ParseContext context = new ParseContext();
-            context.set(Parser.class,parser);
-            parser.parse(input,handler, metadata,context);
-            try {
-                metadataToString(metadata);
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            content = handler.toString();
-        } catch (IOException e) {
-            throw new IllegalArgumentException("file not found：");
-        } catch (TikaException e) {
-            throw new IllegalArgumentException("can't read file through Tika：");
-        } catch (SAXException e) {
-            System.out.println("不能解析");
-//			throw new IllegalArgumentException("can't parse file through Tika：");
-        }
-        return content;
-
-    }
-    private String metadataToString(Metadata m) throws Exception {
-
-        StringBuilder metadataBuffer = new StringBuilder();
-
-        String[] names = m.names();
-        Arrays.sort(names);
-        for (String name : names) {
-            metadataBuffer.append(name);
-            metadataBuffer.append(": ");
-            metadataBuffer.append(m.get(name));
-            metadataBuffer.append("\n");
-        }
-
-        return metadataBuffer.toString();
+    private Logger log = Logger.getLogger(this.getClass());
+    /**
+     * tika读取文件
+     */
+    public String read(File file) throws IOException, TikaException, SAXException {
+        /**
+         * 设置input
+         */
+        InputStream input = new FileInputStream(file);
+        /**
+         * 设置meta
+         */
+        Metadata metadata = new Metadata();
+        metadata.set(Metadata.CONTENT_ENCODING, "utf-8");
+        metadata.set(Metadata.RESOURCE_NAME_KEY, file.getName());
+        /**
+         * 设置handler
+         */
+        ContentHandler handler = new BodyContentHandler();//当文件大于100000时，new BodyContentHandler(1024*1024*10);
+        /**
+         * 设置context
+         */
+        ParseContext context = new ParseContext();
+        Parser parser = new AutoDetectParser();  // 自动解析器
+        context.set(Parser.class, parser);
+        /**
+         * tika读取
+         */
+        parser.parse(input, handler, metadata, context);
+        return handler.toString();
     }
 
     @Override
     public String getContent(File file) {
-        return null;
+        log.info("读取文件： " + file.getAbsoluteFile());
+        String content = "";
+        try {
+            content = read(file);
+        } catch (IOException e) {
+            log.error("not found file: " + file.getAbsolutePath());
+            log.error(e, e.fillInStackTrace());
+        } catch (TikaException e) {
+            log.error("tika read error: " + file.getAbsolutePath());
+            log.error(e, e.fillInStackTrace());
+        } catch (SAXException e) {
+            log.error("tika read error: " + file.getAbsolutePath());
+            log.error(e, e.fillInStackTrace());
+        }
+        return content;
+    }
+
+    public static void main(String[] args){
+        Read read = new TikaReader();
+        File file = new File("C:\\Users\\TEMP.XZ-20131014UNWI.011\\Desktop\\智联张守兵_高级JAVA开发..._中文_20150226_65549432.doc");
+        System.out.println(read.getContent(file));
     }
 }
