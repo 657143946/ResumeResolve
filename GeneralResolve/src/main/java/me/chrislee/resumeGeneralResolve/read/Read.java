7b1b027b57612c7807e32a3fcec1b7e840325516
chrislee <657143946@qq.com>
2015-07-09 17:19:32 +0800

    注释

--- GeneralResolve/src/main/java/me/chrislee/resumeGeneralResolve/read/Read.java
+++ GeneralResolve/src/main/java/me/chrislee/resumeGeneralResolve/read/Read.java
@@ -4,7 +4,14 @@ import java.io.File;
 
 /**
  * Created by ChrisLee.
+ * 读取文档内容的接口
  */
 public interface Read {
+    /**
+     * 读取文档内容接口
+     *
+     * @param file File 要读取的文件
+     * @return String 文件内容
+     */
     public String getContent(File file);
 }
