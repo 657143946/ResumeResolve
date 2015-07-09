bdb2e4eb9cd7ab33e7d410c1ab4121e65ecc3444
chrislee <657143946@qq.com>
2015-07-09 14:47:26 +0800

    注释

--- GeneralResolve/src/main/java/me/chrislee/resumeGeneralResolve/preprocess/Preprocess.java
+++ GeneralResolve/src/main/java/me/chrislee/resumeGeneralResolve/preprocess/Preprocess.java
@@ -6,7 +6,10 @@ package me.chrislee.resumeGeneralResolve.preprocess;
  */
 public interface Preprocess {
     /**
-     * 文本预处理格式化
+     * 预处理接口
+     *
+     * @param content String 待处理的文本
+     * @return String 处理后的文本
      */
     public String preprocess(String content);
 }
